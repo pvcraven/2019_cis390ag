98ae55fec7e5b8e62aa31f1b78af45cf7337139f
GeoSau <saucedogeorge9@gmail.com>
2019-10-14 15:45:28 -0500

    Changed the Zombie Spawn Rate and should not be more playable instead of just dying

--- Unity Project/Assets/Scripts/LogicControllers/ZombieSpawner.cs
+++ Unity Project/Assets/Scripts/LogicControllers/ZombieSpawner.cs
@@ -17,13 +17,13 @@ public class ZombieSpawner : MonoBehaviour {
 
         //If we add more animations to the other zombie we can have different kinds of zombies spawn
 
-        if(zombieNumber == 0 || zombieNumber == 2 || zombieNumber == 4)
+        if(zombieNumber == 0 || zombieNumber == 1 || zombieNumber == 2)
         {
             GameObject zombie = UnityEngine.Object.Instantiate(zombiePrefab[zombieNumber], zombieSpawner.position, Quaternion.identity);
 
-            UnityEngine.Object.Destroy(zombie, 30.0f);
+            UnityEngine.Object.Destroy(zombie, 15.0f);
         }
-        else if(zombieNumber == 3 || zombieNumber == 1)
+        else if(zombieNumber == 2 || zombieNumber == 1)
         {
             GameObject zombie = UnityEngine.Object.Instantiate(zombiePrefab[zombieNumber], zombieSpawner.position, Quaternion.identity);
             UnityEngine.Object.Destroy(zombie, 15.0f);
